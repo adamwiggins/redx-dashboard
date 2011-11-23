7896618a664aac50192332a7f194cc25cf7aef9e
Mark McGranaghan <mmcgrana@gmail.com>
2011-11-23 03:54:26 -0800

    more event handling

--- gen.js
+++ gen.js
@@ -7,8 +7,29 @@ var options = require("url").parse(url);
     options.path   = options.pathname;
     options.host   = options.hostname;
 
+console.log("connect");
 var req = ((options.protocol == "https:" ? https : http)).request(options);
 
 setInterval(function() {
+  console.log("emit");
   req.write(JSON.stringify({"timestamp": "2011-08-03T:58:10+00:00", "source": "heroku", "ps": "router", "msg": "GET wombat.heroku.com/ dyno=web.1 queue=0 wait=0ms service=11ms status=200 bytes=2"}) + "\n");
-}, 100)
+}, 100);
+
+req.on("response", function(res) {
+  console.log("response");
+
+  res.on("error", function() {
+    console.log("error");
+    process.exit(1);
+  });
+
+  res.on("end", function() {
+    console.log("end");
+    process.exit(1);
+  });
+
+  res.on("close", function() {
+    console.log("close");
+    process.exit(1);
+  });
+});
